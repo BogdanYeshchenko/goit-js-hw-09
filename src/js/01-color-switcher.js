b6d8c764afebcec58057717b3c858f1a88d12317
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-17 13:41:45 +0200

    add task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,33 @@
+const el = {
+  startBtn: document.querySelector('[data-start]'),
+  stopBtn: document.querySelector('[data-stop]'),
+  body: document.querySelector('body'),
+};
+let intervalChangeColor = null;
+let isChangeColorWork = false;
 
+el.startBtn.addEventListener('click', handleStartChangeColor);
+el.stopBtn.addEventListener('click', handleStopChangeColor);
+
+function handleStartChangeColor() {
+  if (isChangeColorWork) {
+    return;
+  }
+
+  isChangeColorWork = true;
+
+  intervalChangeColor = setInterval(changeColorBody, 1000);
+}
+
+function handleStopChangeColor() {
+  isChangeColorWork = false;
+  clearInterval(intervalChangeColor);
+}
+
+function changeColorBody() {
+  el.body.style.backgroundColor = getRandomHexColor();
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
