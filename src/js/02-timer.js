d2e96cdbc68be3decdb668f77747ef5d8b7639d5
BogdanYeshchenko <b.yeshchenko@gmail.com>
2023-02-17 23:20:56 +0200

    add task 2

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,16 +1,6 @@
 import flatpickr from 'flatpickr';
 
-import 'flatpickr/dist/flatpickr.min.css';
-
-const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    console.log(selectedDates[0]);
-  },
-};
+import '../../node_modules/flatpickr/dist/flatpickr.min.css';
 
 const el = {
   input: document.querySelector('#datetime-picker'),
@@ -21,4 +11,77 @@ const el = {
   seconds: document.querySelector('[data-seconds]'),
 };
 
+let timerID = null;
+let selectedTime = 0;
+let isTimerOn = false;
+
+el.startBtn.setAttribute('disabled', true);
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    selectedTime = selectedDates[0].getTime();
+
+    if (selectedTime > new Date().getTime()) {
+      el.startBtn.removeAttribute('disabled');
+      return;
+    }
+
+    el.startBtn.setAttribute('disabled', true);
+    alert("Ти погано людина, 'РЕДИСКА'. Please choose a date in the future");
+  },
+};
+
 const d = flatpickr('#datetime-picker', options);
+
+el.startBtn.addEventListener('click', handleStartTimer);
+
+function handleStartTimer() {
+  if (isTimerOn) {
+    return;
+  }
+
+  timerID = setInterval(Timer, 1000);
+}
+
+function Timer() {
+  let timeLeft = Math.round(selectedTime) - Math.round(new Date().getTime());
+  isTimerOn = true;
+
+  if (timeLeft < 0) {
+    isTimerOn = false;
+    clearInterval(timerID);
+    return;
+  }
+
+  assingValue(convertMs(timeLeft));
+}
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = String(Math.floor(ms / day)).padStart(2, '0');
+  const hours = String(Math.floor((ms % day) / hour)).padStart(2, '0');
+  const minutes = String(Math.floor(((ms % day) % hour) / minute)).padStart(
+    2,
+    '0'
+  );
+  const seconds = String(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  ).padStart(2, '0');
+
+  return { days, hours, minutes, seconds };
+}
+
+function assingValue({ days, hours, minutes, seconds }) {
+  el.days.textContent = days;
+  el.hours.textContent = hours;
+  el.minutes.textContent = minutes;
+  el.seconds.textContent = seconds;
+}
